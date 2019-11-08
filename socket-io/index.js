09748a059e0646db37c49a729e4d52ba425f379b
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2019-11-08 14:16:06 +0700

    fix socket origins bug

--- socket-io/index.js
+++ socket-io/index.js
@@ -2,8 +2,8 @@ const handlers = require('./eventHandlers');
 
 let io = null;
 let initializeIO = server => {
-    const io = require('socket.io').listen(server);
-    io.origins(['http://localhost:3000']);
+    io = require('socket.io').listen(server);
+    //io.origins(['http://localhost:3000']);
     io = handlers(io);
 }
 
